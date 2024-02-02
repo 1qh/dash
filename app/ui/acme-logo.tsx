6535f48218bd04ba7b4d0ce1d36e79ba0593818e
Huy <laiquanghuy24122001@gmail.com>
2024-02-02 20:56:42 +0700

    .

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,10 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { RectangleGroupIcon } from '@heroicons/react/24/solid'
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+    <div className='flex flex-row items-center leading-none text-white w-60'>
+      <RectangleGroupIcon className='h-12 w-12' />
+      <p className='text-5xl mx-3'>Brick AI</p>
     </div>
-  );
+  )
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter_Tight, Playfair_Display } from 'next/font/google'
+
+export const inter = Inter_Tight({ subsets: ['latin'] })
+
+export const playfair = Playfair_Display({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
